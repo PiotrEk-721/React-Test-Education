a8859e7088a0ccde9537d24cbd3b520a003b06cd
Piotr Falkowski <piotrfalkowski721@gmail.com>
2025-08-23 22:48:09 +0200

    15.2. Tests for convertPLNToUSD.js

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,15 +1,14 @@
-import PropTypes from 'prop-types';
-import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
-import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
-import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
-import { useMemo } from 'react';
-import styles from './ResultBox.module.scss';
+import PropTypes from "prop-types";
+import { convertUSDToPLN } from "./../../utils/convertUSDToPLN";
+import { convertPLNToUSD } from "./../../utils/convertPLNToUSD";
+import { formatAmountInCurrency } from "./../../utils/formatAmountInCurrency";
+import { useMemo } from "react";
+import styles from "./ResultBox.module.scss";
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (from === "USD" && to === "PLN") return convertUSDToPLN(amount);
+    if (from === "PLN" && to === "USD") return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
@@ -26,6 +25,6 @@ ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-}
+};
 
 export default ResultBox;
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,22 @@
 export const convertPLNToUSD = (PLN) => {
+  if (PLN === null || typeof PLN === "function" || typeof PLN === "object") {
+    return "Error";
+  }
+
+  if (typeof PLN === "string") {
+    return NaN;
+  }
+
+  if (!Number.isFinite(PLN)) return NaN;
+
+  if (PLN < 0) PLN = 0;
 
   const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
   });
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, " ");
+};
