fa6be5d222236cc7a0d50e2e4a6cb12706b82537
Piotr Falkowski <piotrfalkowski721@gmail.com>
2025-08-24 02:55:24 +0200

    15.3. add test for CurrencyForm that elements was rendering

--- src/components/CurrencyForm/CurrencyForm.js
+++ src/components/CurrencyForm/CurrencyForm.js
@@ -1,40 +1,40 @@
-import { useState } from 'react';
-import TextInput from './../TextInput/TextInput';
-import Select from './../Select/Select';
-import Button from './../Button/Button';
-import styles from './CurrencyForm.module.scss';
+import { useState } from "react";
+import TextInput from "./../TextInput/TextInput";
+import Select from "./../Select/Select";
+import Button from "./../Button/Button";
+import styles from "./CurrencyForm.module.scss";
 
 const CurrencyForm = ({ action }) => {
-  const [amount, setAmount] = useState('');
-  const [from, setFrom] = useState('PLN');
-  const [to, setTo] = useState('PLN');
+  const [amount, setAmount] = useState("");
+  const [from, setFrom] = useState("PLN");
+  const [to, setTo] = useState("PLN");
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    action({ 
+    action({
       amount: parseInt(amount),
       from,
       to,
     });
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <label>
         <span>Amount:</span>
-        <TextInput type="number" value={amount} onChange={e => setAmount(e.target.value)} />
+        <TextInput type="number" value={amount} onChange={(e) => setAmount(e.target.value)} data-testid="amount" />
       </label>
       <label>
         <span>From</span>
-        <Select onChange={e => setFrom(e.target.value)}>
+        <Select onChange={(e) => setFrom(e.target.value)} data-testid="from-select" >
           <option value="PLN">PLN</option>
           <option value="USD">USD</option>
         </Select>
       </label>
       <label>
         <span>To</span>
-        <Select onChange={e => setTo(e.target.value)}>
+        <Select onChange={(e) => setTo(e.target.value)} data-testid="to-select" >
           <option value="PLN">PLN</option>
           <option value="USD">USD</option>
         </Select>
